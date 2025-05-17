707a069998d74cd42981f48d4a7d3f7d5fbba883
ʙᴜʙᴏ <31972842+JeudiD@users.noreply.github.com>
2025-05-16 23:50:28 -0700

    Add files via upload

---
+++ week5/week5/src/dailySpecials.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class dailySpecials {
+
+    public static void main(String[] args) {
+
+        String specials;
+        String coffee = "";
+        double price = 0.0;
+
+        Scanner input  = new Scanner(System.in);
+
+        System.out.println("Please enter the day of the week excluding weekends (Monday - Friday only!)");
+
+        specials = input.next();
+
+        //input.close();
+        boolean saturday = specials.equals("Saturday");
+        boolean sunday = specials.equals("Sunday");
+
+
+        if (saturday || sunday) {
+            System.out.println("Please enter a weekday, not weekend!");
+            specials = input.next();
+
+
+        }
+
+
+
+
+        switch (specials) {
+            case "Monday" -> {
+                coffee = "Latte";
+                price = 4.95;
+                System.out.println(specials + "'s coffee of the day is " + coffee + " and the price will be $" + price);
+            }
+            case "Tuesday" -> {
+                coffee = "Frapp";
+                price = 5.95;
+                System.out.println(specials + "'s coffee of the day is " + coffee + " and the price will be $" + price);
+            }
+            case "Wednesday" -> {
+                coffee = "Cappuccino";
+                price = 4.35;
+                System.out.println(specials + "'s coffee of the day is " + coffee + " and the price will be $" + price);
+            }
+            case "Thursday" -> {
+                coffee = "Regular Joe";
+                price = 2.95;
+                System.out.println(specials + "'s coffee of the day is " + coffee + " and the price will be $" + price);
+            }
+            case "Friday" -> {
+                coffee = "Green Tea Latte";
+                price = 6.95;
+                System.out.println(specials + "'s coffee of the day is " + coffee + " and the price will be $" + price);
+            }
+            default -> {
+                System.out.println("Please enter a valid day or check your spelling!");
+            input.close();
+            return;
+            }
+        }
+
+
+        System.out.println("How many " +coffee+ " would you like to order?");
+
+        int quantity = input.nextInt();
+        double total = quantity * price;
+
+
+        System.out.println(quantity+ " " +coffee+ " have been ordered totalling");
+        System.out.printf("$%.2f dollars\n", total);
+
+
+
+
+
+    }
+
+
+}
